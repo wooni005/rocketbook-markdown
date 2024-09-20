27db08c938ca888a92c01b1298cf44045be52db2
Arjan <arjan@wooning.cz>
2024-09-20 10:57:00 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,213 @@
-# rocketbook-markdown
-Converts automatically your Rocketbook notes from email into Markdown
+# Rocketbook to Markdown
+
+This script is waiting for a Rocketbook email with a PDF and a OCR text attachment. When receiving an email it converts the contents automatically to Markdown and puts it in the right notes folder.
+
+## What is a Rocketbook?
+
+A Rocketbook is a reusable notebook that combines traditional handwriting with digital technology. You write on the pages with a special pen (Pilot Frixion), and then use a smartphone app to scan your notes and save them to the cloud. The pages can be erased with water or a microwave, making them endlessly reusable. It's a great way to stay organized while reducing paper waste.
+
+## How does it work?
+
+The script reads the Rocketbook email, which contains a PDF and an OCR text attachment.
+The subject of the email is used for the filename of the note, the tags between brackets are used to store the content in the correct notes folder.
+The script converts the PDF to Markdown and puts it in the right folder. Also the PDF will be moved to this folder with the same filename. At the top of the Markdown file is a link to the original PDF.
+
+Why is **IMAP IDLE** used? With the IMAP IDLE command, the conversion is responding immediately. It is also possible to check the mail on polling base, but to many requests to the IMAP server is not recommended. Your account can be suspended if you put the delay time too short. So IMAP IDLE is ideal, it is constantly waiting for a new mail. If you have trouble with the IDLE command, you can disable it in the configuration with FORCE_DISABLE_IDLE_COMMAND = True.
+
+TODO:
+- Sample input PDF and output
+- Tijd uit filename halen?
+- config.py in .gitignore zetten
+
+## Installation
+
+```bash
+# Go to your home directory
+$ cd
+
+# Clone the repository and enter the directory
+$ git clone https://github.com/wooni005/rocketbook-markdown.git
+$ cd rocketbook-markdown
+
+# Make the script executable
+$ chmod +x rocketbook-markdown.py
+```
+
+Install the required Python3 modules:
+```bash
+$ sudo pip3 install imaplib2 email python-markdown html2text
+```
+
+## Configuration
+
+Create a configuration file in the `config.py` file with the example file as a template:
+
+```bash
+$ cd rocketbook-markdown
+$ cp config.py.example config.py
+```
+
+Fill in your IMAP mailserver settings and the corresponding credentials to read the IMAP folder and fill in the other settings for the conversion process.
+
+```bash
+$ nano config.py
+
+# Mail settings
+IMAP_SERVER = 'imap.gmail.com'
+IMAP_PORT = 993
+USERNAME = 'your@email'
+PASSWORD = 'yourPassword'
+MAILBOX_FOLDER = "Rocketbook"
+MAIL_CHECK_INTERVAL = 60 # [sec]
+FORCE_DISABLE_IDLE_COMMAND = False
+
+# Conversion settings
+CONFIG_REMOVE_LINE_ENDINGS = True
+TAG_TO_PATH_CONVERSION = {
+    'Rocketbook': 'notes/rocketbook', # Default tag with default path
+    'YourTag1': '/your/path/for/your/notes_tag1',
+    'YourTag2': '/your/path/for/your/notes_tag2',
+}
+```
+
+## Configuration settings
+
+**IMAP_SERVER**: IMAP server address.
+**IMAP_PORT**: IMAP server SSL port.
+**USERNAME**: IMAP username. Your email adress at this mail server.
+**PASSWORD**: IMAP password. When using Gmail, generate a new app password here: https://myaccount.google.com/apppasswords
+
+**MAILBOX_FOLDER**: IMAP folder. The default value is "Rocketbook", make sure all email from Rocketbook is moved to this folder. If you put here "INBOX", all incoming emails will be processed, also the not Rocketbook emails. Make a filter in your mailbox to achieve this.
+
+**MAIL_CHECK_INTERVAL** is the time in seconds between each check of the IMAP folder. The default value is 60 seconds.
+Remark! Don't use a too short interval, risk that your account will be suspended.
+
+**FORCE_DISABLE_IDLE_COMMAND**: Set to True if you want to disable the IDLE command. The default value is False.
+Use this when you have trouble with the IDLE command and no updates are received. See Tips and Tricks->Google Workspace
+
+**CONFIG_REMOVE_LINE_ENDINGS**: Set to True if you want to remove line endings from the converted files. The default value is True.
+In case you want to keep line endings, like in the notes, set it to False.
+
+**TAG_TO_PATH_CONVERSION**: Set the tags and their corresponding paths. The default value is the Rocketbook tag with the default path.
+
+## Rocketbook Destination Settings
+
+Open de Rocketbook application and perform the following settings:
+
+- Settings - Handwriting Recognition (OCR)
+  - Smart Titles: Enable
+  - Use Smart Titles to group scans: Disable
+  - Smart Search: Enable
+  - Smart lists: Disable (Is making automatically todo lists for you)
+  - Smart Tags: Disable (Is only for Rocketbook Pro pages)
+- File Naming Template: Page-Time
+- Email Naming Template: Filename-Time
+
+Destinations: Pick one of the icons you want to use for this script:
+- Change Recipient: 'your@email' (Your email address which is used in the script)
+- Change Destination: Email
+- File Type: PDF
+- Bundle Scans: Enable
+- OCR Transription: Enable
+  - Attach: Enable
+- Send Smart Tag: Enable
+- Smart Tag Subject Line: Enable
+
+## Testing the script
+
+```bash
+# Put on the DEBUG mode
+$ nano rocketbook-markdown.py
+...
+DEBUG = True
+...
+
+# Run the script
+$ python3 rocketbook-markdown.py
+```
+
+## Run the script as a service
+
+If everything works nicely, you can run the script as a service.
+Remark: Make sure that you switch off the DEBUG mode before running the service.
+
+```bash
+$ cd rocketbook-markdown
+# Copy the service file to the system
+$ sudo cp rocketbook-markdown.service /etc/systemd/system
+
+# Reload the service file
+$ systemctl daemon-reload
+
+# Enable and start the service
+$ systemctl enable rocketbook-markdown
+$ systemctl start rocketbook-markdown
+```
+
+## OCR tips for better results
+
+For OCR software it is not easy to read your handwriting. Here are some tips for better results.
+
+1. Take a clear and stable image with enough light to get high contrast
+2. Place the Rocketbook notes on a flat surface and take the picture right above the Rocketbook and not in an angle
+3. Use block letters in your Rocketbook notes
+4. Keep a open line between two written lines if the software is mixing lines
+5. Keep the surface clean, leaving some rest after erasing a mistake gives strange results
+6. Try other scan settings for better results, menu: Settings > Scan Settings > Rocketbook Enhancement Type
+
+Here are some other tips:
+- https://getrocketbook.com/blogs/news/rocket-labs-002-improve-formatting-in-ocr-transcription
+- https://youtu.be/R3BSJtiiCIo
+
+## Tips for improving the conversion process
+
+1. Use the Smart Titles feature in Rocketbook, the Smart Titles are used for the filename, otherwise the default is `Rocketbook`
+2. Tags are used for to store the notes in the correct folder, the default is the Rocketbook folder, see config.py
+
+## Use Markdown in the Rocketbook notes
+
+It is possible to use Markdown format already in your notes, so you don't need to edit them afterwards.
+
+For example you use the following Markdown syntax in your Rocketbook handwritten notes:
+
+- Headings: H1='#', H2='##', H3='###', etc
+- Bold: **bold**
+- Italic: _italic_ or *italic*
+- Strikethrough (~): ~~strikethrough~~
+- Code: (`) `code`
+- Links: `[text](url)`
+- Images: `![alt text](url)`
+
+To get also a drawing from your Rocketbook notes, I'm taking a screenshot of the drawing in the PDF and past that in the Markdown file.
+
+## OCR processing locally
+
+My plan was to add the possibility to scan with JPG do the OCR locally, but the Rocketbook OCR is much more precise.
+Also tested some more OCR tools, but never better than the Rocketbook one.
+
+```bash
+$ sudo apt-get install tesseract-ocr libssl-dev
+$ sudo pip3 install Pillow pytesseract opencv-python
+```
+
+Then run the script with the JPG file as input.
+
+```python
+import pytesseract
+from PIL import Image
+
+# Open the JPEG image file
+image = Image.open('image.jpg')
+
+# Process the image with Tesseract
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
+```
+
+# Tips and tricks
+
+## Google Workspace
+
+I'm using Google Workspace and I couldn't get the IDLE mode working. When testing on capability, it returns that the IDLE command is supported, but it doesn't work. It waits forever on the IDLE command and doesn't report that a new email is received. I'm not sure why, I checked all settings in the IMAP admin settings, everything seems to be set OK. Please use a normal Gmail account and forward it to your Google Workspace email account if you like to have it there, but it is not necessary.
---
+++ rocketbook-markdown.py
@@ -0,0 +1,239 @@
+import time
+import os
+import imaplib2
+import datetime
+import email
+import markdown
+import html2text
+import re
+import config
+
+DEBUG = False
+idle_command = False
+mail = None
+
+def print_debug(text):
+    if DEBUG:
+        print(text)
+
+def move_file_to_folder_and_check_if_exists(filename_org, target_folder_name, note_title, note_date):
+    if not os.path.exists(os.path.join(target_folder_name, filename_org)):
+        # If the file doesn't exist in the target folder, move it to the folder
+        os.replace(filename_org, os.path.join(target_folder_name, filename_org))
+    else:
+        # If the file exists, add a number to the end of the filename
+        count = 1
+        filename_new = f"{note_title}-{note_date}-{count}.md"
+        while os.path.exists(os.path.join(target_folder_name, filename_new)):
+            count += 1
+            filename_new = f"{note_title}-{note_date}-{count}.md"
+        os.replace(filename_org, os.path.join(target_folder_name, filename_new))
+
+def process_mail_with_notes(mail):
+    if mail is None:
+        return
+
+    status, messages = mail.search(None, '(UNSEEN)')
+
+    print_debug(f"Status: {status} Number of unread messages: {len(messages[0].split())}")
+
+    if status == 'OK':
+        # Loop door de emails
+        for num in messages[0].split():
+            status, msg = mail.fetch(num, '(RFC822)')
+            raw_message = msg[0][1]
+
+            # Parse de email
+            message = email.message_from_bytes(raw_message)
+
+            subject = message['Subject']
+            print_debug(subject)
+
+            match_title = re.match(r'^(.+?)-(\d+\.\d+\.\d+) \[.*?\]', subject)
+            if match_title is not None:
+                # Regex found a match
+                note_title = match_title.group(1)
+                # Check if there is a note title found in subject
+                note_date = match_title.group(2)
+            else:
+                # No match with the subject default the title to Rocketbook with timestamp HH.MM.SS
+                note_date = datetime.datetime.now().strftime('%H.%M.%S')
+                note_title = f'Rocketbook-{note_date}'
+
+            # Get the tags from the subject
+            tags = re.findall(r'\[(.*?)\]', subject)
+
+            # if no tags found default to Rocketbook
+            if len(tags) == 0:
+                tags = ['Rocketbook']
+
+            print_debug(f"Note title: {note_title}, tag(s): {tags} " )
+
+            # Check if there is an 'application/plain' attachment
+            ocr_text_source = 'text/html' # Default, the text is in the email body and not as an attachment
+            for part in message.walk():
+                if part.get_content_type() == 'application/plain':
+                    # Yeah, there is a plain text attachment with the OCR
+                    ocr_text_source = 'application/plain'
+
+            # Loop through the attachments
+            for part in message.walk():
+                if part.get_content_type() == ocr_text_source:
+                    print_debug("OCR text source: " + part.get_content_type())
+                    # Get the attachment
+                    attachment = part.get_payload(decode=True)
+                    text = attachment.decode('utf-8')
+
+                    # Convert plain text to Markdown which contains HTML
+                    html_text = markdown.markdown(text)
+                    # print_debug(html_text)
+
+                    if config.CONFIG_REMOVE_LINE_ENDINGS:
+                        h = html2text.HTML2Text()
+                        h.ignore_images = True
+                        html_text2 = h.handle(html_text)
+
+                    # Improve the Markdown output
+                    markdown_text = html_text2.replace('<h1>', '# ').replace('</h1>', '')
+                    markdown_text = html_text2.replace('<h2>', '## ').replace('</h2>', '')
+                    markdown_text = html_text2.replace('<h3>', '### ').replace('</h3>', '')
+                    markdown_text = html_text2.replace('<p>', '\n').replace('</p>', '\n')
+                    markdown_text = html_text2.replace('<strong>', '**').replace('</strong>', '**')
+                    markdown_text = html_text2.replace('<em>', '*').replace('</em>', '*')
+                    markdown_text = html_text2.replace('<br>', '\n')
+
+                    # Make a link at the top of the page in the markdown text to the pdf file
+                    pdf_file = None
+                    for part in message.walk():
+                        if part.get_content_type() == 'application/pdf':
+                            pdf_file = part.get_payload(decode=True)
+                            break
+                    if pdf_file:
+                        pdf_filename = f"{note_title}-{note_date}.pdf"
+                        with open(pdf_filename, 'wb') as f:
+                            f.write(pdf_file)
+                        time_format = note_date.replace('.', ':')
+                        markdown_text = config.get_pdf_link_text(time_format, pdf_filename, pdf_filename) + markdown_text
+
+                    # Store the markdown in a file
+                    markdown_filename = f"{note_title}-{note_date}.md"
+                    with open(markdown_filename, 'w') as f:
+                        f.write(markdown_text)
+
+                    if DEBUG:
+                        # Store the HTML in a file for debugging/checking
+                        html_filename = f"{note_title}-{note_date}.html"
+                        with open(html_filename, 'w') as f:
+                            f.write(html_text)
+                    else:
+                        html_filename = None
+
+                    # Move the markdown file to the correct folder
+                    folder_name = tags[0] # Take the first tag for the folder
+                    if folder_name in config.TAG_TO_PATH_CONVERSION:
+                        folder_name = config.TAG_TO_PATH_CONVERSION[folder_name]
+
+                    # Check if the folder exists
+                    if not os.path.exists(folder_name):
+                        # Create the folder
+                        os.makedirs(folder_name)
+
+                    # Move the Markdown file
+                    move_file_to_folder_and_check_if_exists(markdown_filename, folder_name, note_title, note_date)
+                    if html_filename:
+                        # Move the HTML file if it exists
+                        move_file_to_folder_and_check_if_exists(html_filename, folder_name, note_title, note_date)
+                    if pdf_file:
+                        # Move the PDF file if it exists
+                        move_file_to_folder_and_check_if_exists(pdf_filename, folder_name, note_title, note_date)
+
+def check_idle_capability(mail):
+    # Send a "CAPABILITY" command
+    cap_response = mail.capability()[1][0].decode('utf-8')
+    # print_debug(cap_response)
+
+    # Check the "IDLE" capability
+    if 'IDLE' in cap_response:
+        print_debug("The server supports IDLE mode")
+        return True
+    else:
+        print_debug("The server does not support IDLE mode")
+        return False
+
+def connect_to_imap():
+    try:
+        mail = imaplib2.IMAP4_SSL(config.IMAP_SERVER, config.IMAP_PORT)
+        mail.login(config.USERNAME, config.PASSWORD)
+        mail.select(config.MAILBOX_FOLDER)
+        return mail
+    except Exception as e:
+        print_debug(f'ERROR while connecting to IMAP-server: {e}')
+        return None
+
+def reconnect_to_imap(mail):
+    try:
+        mail.logout()
+        mail = connect_to_imap()
+        return mail
+    except Exception as e:
+        print_debug(f'ERROR while reconnecting to IMAP-server: {e}')
+        return None
+
+def main():
+    global mail
+
+    while True:
+        if mail is None:
+            mail = connect_to_imap()
+            if mail is None:
+                # Connect failed, sleep for a while and try again
+                time.sleep(config.MAIL_CHECK_INTERVAL)
+            else:
+                if not config.FORCE_DISABLE_IDLE_COMMAND:
+                    idle_command = check_idle_capability(mail)
+                else:
+                    # For example for Google Workspace, the server does not work well with IDLE
+                    idle_command = False
+
+        if mail is not None:
+            try:
+                if not idle_command:
+                    # Not using IDLE, so connect to the server
+                    mail = connect_to_imap()
+                else:
+                    # Using IDLE, so wait for new mail
+                    print_debug("Wait for new mail with IDLE command")
+                    status, data = mail.idle()
+
+                    if status == 'OK':
+
+                        print_debug("Server connection OK")
+                    else:
+                        print_debug("Server connection not OK, send NOOP command to test the server connection")
+                        response = mail.noop()
+                        print_debug(f"NOOP response: {response}")
+
+                    print_debug(f"typ: {status} data: {data}")
+                
+                process_mail_with_notes(mail)
+
+                if idle_command:
+                    # When using IDLE, we need to send DONE to the server
+                    print_debug("Let the server now the processing is done")
+                    mail.send(b'DONE\r\n')
+                else:
+                    # When not using IDLE, close the connection and sleep for a while
+                    mail.close()
+                    mail.logout()
+                    print_debug("No IDLE command just sleep for a while")
+                    time.sleep(config.MAIL_CHECK_INTERVAL)
+
+            except Exception as e:
+                print_debug(f'ERROR while getting notes from IMAP-server: {e}')
+                mail = reconnect_to_imap(mail)
+                if mail is None:
+                    # Reconnect failed, sleep for a while and try again
+                    time.sleep(config.MAIL_CHECK_INTERVAL)
+
+if __name__ == '__main__':
+    main()
